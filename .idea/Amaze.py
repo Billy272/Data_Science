34d98de18ba10973cb5e90dd728b08a68b8e1024
Bill Bitok <billylaroi@gmail.com>
2023-05-25 09:40:12 +0300

    Commit

---
+++ .idea/Amaze.py
@@ -0,0 +1,3 @@
+print = run_n_times(20)(print)
+
+print('What is happening?!?!')
---
+++ .idea/htmltag.py
@@ -0,0 +1,9 @@
+# Wrap the result of hello_goodbye() in <div> and </div>
+@html('<div>', '</div>')
+def hello_goodbye(name):
+    return '\n{}\n{}\n'.format(hello(name), goodbye(name))
+
+print(hello_goodbye('Alice'))
+
+
+
