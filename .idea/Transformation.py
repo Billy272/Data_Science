7a0e8b39cec94f35395937a83fac57457b62ef2b
Bill Bitok <billylaroi@gmail.com>
2023-05-25 17:21:22 +0300

    Commit

---
+++ .idea/Transformation.py
@@ -0,0 +1,15 @@
+ad_conversion["qdrt_n_impressions"] = ad_conversion["n_impressions"] ** 0.25
+ad_conversion["qdrt_n_clicks"] = ad_conversion["n_clicks"] ** 0.25
+
+mdl_click_vs_impression = ols("qdrt_n_clicks ~ qdrt_n_impressions", data=ad_conversion, ci=None).fit()
+
+explanatory_data = pd.DataFrame({"qdrt_n_impressions": np.arange(0, 3e6+1, 5e5) ** .25,
+                                 "n_impressions": np.arange(0, 3e6+1, 5e5)})
+
+# Complete prediction_data
+prediction_data = explanatory_data.assign(
+    qdrt_n_clicks = mdl_click_vs_impression.predict(explanatory_data)
+)
+
+# Print the result
+print(prediction_data)
