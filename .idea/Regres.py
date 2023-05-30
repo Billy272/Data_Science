9d43a2a537e51680074e67b4b4726482a1235bee
Bill Bitok <billylaroi@gmail.com>
2023-05-30 20:54:08 +0300

    Commit

--- .idea/Regres.py
+++ .idea/Regres.py
@@ -9,3 +9,15 @@ x = x.values.reshape(-1,1)
 #Print shape of x and y
 print("Shape of x:", x.shape)
 print("Shape of y:", y.shape)
+
+# Import LinearRegression
+from sklearn.model import LinearRegression
+
+# Create the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(x,y)
+
+#Make prediction
+prediction_space = np.linspace(min(x), max(x)).reshape(-1,1)
